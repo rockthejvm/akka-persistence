301de92d9230361c2c92b223b9824f235c370a9b
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2018-12-17 16:53:04 +0200

    persist async

---
+++ src/main/scala/part2_event_sourcing/PersistAsyncDemo.scala
@@ -0,0 +1,63 @@
+package part2_event_sourcing
+
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import akka.persistence.PersistentActor
+
+object PersistAsyncDemo extends App {
+
+
+  case class Command(contents: String)
+  case class Event(contents: String)
+
+  object CriticalStreamProcessor {
+    def props(eventAggregator: ActorRef) = Props(new CriticalStreamProcessor(eventAggregator))
+  }
+
+  class CriticalStreamProcessor(eventAggregator: ActorRef) extends PersistentActor with ActorLogging {
+
+    override def persistenceId: String = "critical-stream-processor"
+
+    override def receiveCommand: Receive = {
+      case Command(contents) =>
+        eventAggregator ! s"Processing $contents"
+        // mutate
+        persistAsync(Event(contents)) /*                          TIME GAP                            */ { e =>
+          eventAggregator ! e
+          // mutate
+        }
+
+        // some actual computation
+        val processedContents = contents + "_processed"
+        persistAsync(Event(processedContents)) /*                          TIME GAP                            */ { e =>
+          eventAggregator ! e
+        }
+    }
+
+    override def receiveRecover: Receive = {
+      case message => log.info(s"Recovered: $message")
+    }
+  }
+
+  class EventAggregator extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(s"$message")
+    }
+  }
+
+
+  val system = ActorSystem("PersistAsyncDemo")
+  val eventAggregator = system.actorOf(Props[EventAggregator], "eventAggregator")
+  val streamProcessor = system.actorOf(CriticalStreamProcessor.props(eventAggregator), "streamProcessor")
+
+  streamProcessor ! Command("command1")
+  streamProcessor ! Command("command2")
+
+  /*
+    persistAsync vs persist
+    - perf: high-throughput environments
+
+    persist vs persisAsync
+    - ordering guarantees
+   */
+
+}
